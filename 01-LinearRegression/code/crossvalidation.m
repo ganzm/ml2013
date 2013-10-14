e16e88f74e95af97b9016d7955953db6764133ea
Matthias Ganz <gaenzele@gmail.com>
2013-10-14 11:16:02 +0200

    add function stub

---
+++ 01-LinearRegression/code/crossvalidation.m
@@ -0,0 +1,24 @@
+function [ meanErr ] = crossvalidation( features, expectedResult, hyperparameter)
+
+%CROSSVALIDATION Summary of this function goes here
+%   Detailed explanation goes here
+% feature(:,1) wäre bsp. cache size
+
+[numSamples, numFeatures] = size(features);
+[numResultSamples, ~] = size(expectedResult);
+
+if numSamples ~= numResultSamples 
+    error 'blub';
+end
+
+
+% loop create buckets
+% do crossvalidation
+
+w = train(features(1:10, expectedResult), hyperparameter);
+% TODO
+% least square fitting
+
+meanErr = 0;
+end
+
---
+++ 01-LinearRegression/code/train.m
@@ -0,0 +1,7 @@
+function [ w ] = train( features, expectedResult, hyperparameter )
+%TRAIN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
