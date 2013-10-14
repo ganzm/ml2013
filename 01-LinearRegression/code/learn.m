1df8247cb83a52d383135834233ee58ac161b4ad
Matthias Ganz <gaenzele@gmail.com>
2013-10-14 22:20:35 +0200

    prepared learn file

--- 01-LinearRegression/code/learn.m
+++ 01-LinearRegression/code/learn.m
@@ -1,39 +1,70 @@
 %% this file may be renamed
 
-trainingdata = '../testdata/training.csv';
-%LEARN Summary of this function goes here
-%   Detailed explanation goes here
+% Settings
+testingdataPath = '../testdata/testing.csv';
+trainingdataPath = '../testdata/training.csv';
+validationdataPath = '../testdata/validation.csv';
 
-disp(['learning from ' trainingdata]);
+outputfilePraefix = 'output-';
+outputfilePostfix = '.txt';
 
 
-% read raw csv data
-data = csvread(trainingdata);
-[nSamples, nColumns] = size(data);
+%% read raw csv data
+trainData = csvread(trainingdataPath);
+testData = csvread(testingdataPath);
+validationData = csvread(validationdataPath);
+
+[n, nColumns] = size(trainingdata);
+
+trainResult = trainData(:,15);
+x = trainData(:, 1:14);
+
+%% prepare feature vector
+
+%% logarithmic features - column 8 to 13 are numbers to the power of 2
+x(:,8) = log(x(:,8));
+x(:,9) = log(x(:,9));
+x(:,10) = log(x(:,10));
+x(:,11) = log(x(:,11));
+x(:,12) = log(x(:,12));
+x(:,13) = log(x(:,13));
 
 %% normalize data
-dMean = repmat(mean(data), nSamples, 1);
-dVar = repmat(var(data), nSamples, 1);
+dMean = repmat(mean(x), n, 1);
+dVar = repmat(var(x), n, 1);
 
-normalizedData = (data - dMean) ./ dVar;
-
-%% plot data
-%compare(training_data, [1 15; 2 15]);
-compare(training_data, [13 15]);
-compare(training_data, [14 15]);
+x = (x - dMean) ./ dVar;
 
 
-%% split them
-result = data(:,15);
-features = data(:,1:14);
+%% perform crossvalidation
+
+% todo vary parameters
+err = 1;
+disp(['Crossvalidation Error:' err]);
 
 
-% logarithmic features
-% column 8 to 13 are numbers to the power of 2
+%% perform learning step with the whole training set
 
-% result varies by 10^x
-result = log(result);
+% todo optain w with dimensinality
+w = rand(nColumns-1,1);
 
+%% compute result
+y = w'*x';
+
+
+
+%% Write output file
+
+path = [outputfilePraefix num2str(err) outputfilePostfix];
+disp(['write result to ' path]);
+
+result = y;
+
+fileID = fopen(path, 'w');
+for item = result
+    fprintf(fileID,'%d\n', item);
+end
+fclose(fileID);
 
 
 
