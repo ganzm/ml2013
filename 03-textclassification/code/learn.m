7d22a7bbfd1d6591a64a1c3e4d4a23044c3ac01c
Matthias Ganz <gaenzele@gmail.com>
2013-11-29 11:49:50 +0100

    preprocessing

--- 03-textclassification/code/learn.m
+++ 03-textclassification/code/learn.m
@@ -1,4 +1,8 @@
 
+
+%% add editing stance algos
+addpath('edit_distances');
+
 %% easy baseline
 baseline_easy = 3973.75;
 
@@ -36,44 +40,16 @@ else
 end
 
 
+%% calculate bag of words
+bagOfWords = cell(0,0);
+bagOfWords = addToBagOfWords(bagOfWords, trainingData);
+bagOfWords = addToBagOfWords(bagOfWords, testingData);
+bagOfWords = addToBagOfWords(bagOfWords, validationData);
 
 
-% %% reshape them
-% X = trainingData(:,1:27);
-% labels = trainingData(:,28);
-% 
-% Xt = testingData;
-% Xv = validationData;
-% 
-% %% plot two features
-% normal = X(labels== 1,:);
-% dis = X(labels == -1,:);
-% 
-% for j=1:27    
-%     [xN, ~] = size(normal);
-%     [xD, ~] = size(dis);
-%     numFeatures = max(xN, xD);
-%     tmp = zeros(numFeatures,2);
-%     tmp(1:xN,1) = normal(:,j);
-%     tmp(1:xD,2) = dis(:,j);
-%     
-%     figure;
-%     scale = linspace(min(min(tmp)), max(max(tmp)), 40);
-%     hist(tmp, scale);
-%     title(['Histogram Feature ' num2str(j) ]);
-% end
-% 
-% %% learn
-% 
-% SVMStruct = svmtrain(X, labels, 'autoscale', true, 'kernel_function', 'rbf', 'rbf_sigma', 0.5);
-% 
-% Y = svmclassify(SVMStruct,X);
-% Yt = svmclassify(SVMStruct,Xt);
-% Yv = svmclassify(SVMStruct,Xv);
-% 
-% %% estimate error
-% 
-% 
-% %% write output
-% writeOutput('validation.csv', Yv);
-% writeOutput('testing.csv', Yt);
+% remove duplicates
+bagOfWords = unique(bagOfWords);
+
+%% create bag of similar words
+bagOfSimilarWords = createBagOfSimilarWords(bagOfWords);
+
